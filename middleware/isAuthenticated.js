c1decbd739d75e4913054b1af821fe157b6261f9
Sarah <sarahjanecalvoconesa@gmail.com>
2022-06-03 18:11:38 +0200

    authentication back ready

---
+++ middleware/isAuthenticated.js
@@ -0,0 +1,27 @@
+const { expressjwt } = require("express-jwt");
+
+const isAuthenticated = expressjwt({
+  secret: process.env.TOKEN_SECRET,
+  algorithms: ["HS256"],
+  requestProperty: "payload",
+  getToken: (req) => {
+    if (req.headers === undefined || req.headers.authorization === undefined) {
+      console.log("sin token");
+      return null;
+    }
+    console.log(req.headers.authorization);
+    const tokenArr = req.headers.authorization.split(" ");
+    const tokenType = tokenArr[0];
+    const token = tokenArr[1];
+
+    if (tokenType !== "Bearer") {
+      console.log("Token invalido");
+      return null;
+    }
+
+    console.log("Token entregado");
+    return token;
+  },
+});
+
+module.export = isAuthenticated;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,88 @@
 const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
 const UserModel = require("../models/User.model.js");
+const jwt = require("jsonwebtoken");
+const isAuthenticated = require("../middleware/isAuthenticated");
+//POST para registar el usuario
 
-//CRUD de User
+router.post("/signup", async (req, res, next) => {
+  const { name, email, password, admin } = req.body;
+
+  //validacion de Backend
+
+  if (!name || !email || !password) {
+    res.status(400).json({ errorMessage: "Campos Incompletos" });
+    return;
+  }
+
+  try {
+    const foundUser = await UserModel.findOne({ email });
+    if (foundUser !== null) {
+      res.status(400).json({ errorMessage: "Usuario ya Registrado" });
+      return;
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+
+    await UserModel.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    res.json("Usuario Creado");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST para verificar las credenciales
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const foundUser = await UserModel.findOne({ email });
+    if (foundUser === null) {
+      res.status(400).json({ errorMessage: "Usuario no registrado" });
+      return;
+    }
+
+    // usuario validado
+    const passwordMatch = await bcryptjs.compare(password, foundUser.password);
+    console.log(passwordMatch);
+
+    if (passwordMatch === false) {
+      res.status(401).json({ errorMessage: "Contraseña Incorrecta" });
+      return;
+    }
+
+    // Sistema de auth. TOKEN
+
+    const payload = {
+      _id: foundUser._id,
+      email: foundUser.email,
+      name: foundUser.name,
+      admin: foundUser.admin,
+    };
+
+    const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "12h",
+    });
+    res.json({ authToken: authToken });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get  para chekear si el TOKEN es valido.
+
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  console.log(req.payload);
+  console.log("Pasando por la ruta, todo bien con el middleware");
+  res.json(req.payload);
+});
 
 module.exports = router;
